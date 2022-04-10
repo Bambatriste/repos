b639ef744bf804d173f8943722b032bf377ef15f
Bambatriste <antoine.edouard@gmail.com>
2022-04-10 14:56:44 +0200

    nginx sudo fix

--- 42/other/inception/srcs/requierements/nginx/launch.sh
+++ 42/other/inception/srcs/requierements/nginx/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t test . && docker run -dit test;
+docker build -t test . && docker run -p 8080:80 test;
 id=$(docker ps | awk 'NR==2{print $1}');
 echo $id;
 docker exec -it $id bash
