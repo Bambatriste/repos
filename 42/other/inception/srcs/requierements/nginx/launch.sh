ee78ae1ebc78743f5dde1f6d404b18658a04d84f
user42 <Vous@exemple.com>
2022-04-14 21:28:56 +0200

    oskour

--- 42/other/inception/srcs/requierements/nginx/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t test . && docker run -p 8080:80 test;
-id=$(docker ps | awk 'NR==2{print $1}');
-echo $id;
-docker exec -it $id bash
-
--- 42/other/inception/srcs/requierements/nginx/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-id=$(docker ps | awk 'NR==2{print $1}');
-echo $id;
-docker stop $id
-
