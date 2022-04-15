6b635eac936b27167366f9d16b7f8c1b5b2c6d4f
user42 <aedouard@mi.fr>
2022-04-15 12:48:41 +0200

    db good?

--- 42/other/inception/srcs/requierements/mariadb/entrypoint.sh
+++ 42/other/inception/srcs/requierements/mariadb/entrypoint.sh
@@ -1,3 +1,4 @@
+set -m
 
 if ! cat /var/lib/mysql/.mariadb_installed 2 > /dev/null; then
   mysql_install_db 2> /dev/null;
@@ -12,20 +13,16 @@ while [ $? -ne 0 ]; do echo "waiting for sql to setup .."; sleep 1; mysqladmin p
 
 echo "PING SUCCESS TO MYSQL";
 
-cd /root/
-
 #export SQL_ROOT_USER=aedouard;
 #export SQL_ROOT_PWD=nelito444;
 #export SQL_BASIC_USER=basic_user;
 #export SQL_BASIC_PWD=bonjour1234;
 #export SQL_DB=wordpress;
 
-if ! cat /var/lib/mysql/.mariadb_configured2 > /dev/null;then
-  mariadb -u root < /tmp/sql_config.sql;
+if ! cat /var/lib/mysql/.mariadb_configured 2> /dev/null;then
+  mysql < /tmp/sql_config.sql;
   touch /var/lib/mysql/.mariadb_configured;
   echo "SQL CONFIG DONE";
 fi
 
-mysqladmin shutdown;
-echo "RESTART SQL";
-mysqld_safe;
+fg
