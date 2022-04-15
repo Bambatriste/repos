dbc63d143002383b49455a88a56249469a55216d
user42 <aedouard@toinou.fr>
2022-04-15 03:10:29 +0200

    merci pour le crash la vm

--- 42/other/inception/srcs/requierements/wordpress/entrypoint.sh
+++ 42/other/inception/srcs/requierements/wordpress/entrypoint.sh
@@ -5,8 +5,8 @@ until mysqladmin -hmariadb -u${SQL_ROOT_USER} -p${SQL_ROOT_PWD} ping && mariadb 
 	sleep 2
 done
 echo "SQL ADMIN REACHED"
-wp core install --url="aedouard.42.fr" --allow-root
+wp core install --url="aedouard.42.fr" --title="first website" --admin_user="${SQL_ROOT_USER}"
+--admin_password="${SQL_ROOT_PWD}" --admin_email="bonjour1234@hotmail.fr" --skip-email --allow-root
 
-php-fpm -F -R
-root
+php-fpm7.3 -F;
 
