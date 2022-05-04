705072026f9a0f16442dc7915b3f8648083d8400
Antoine EDOUARD <aedouard@e2r5p12.clusters.42paris.fr>
2022-05-04 18:44:43 +0200

    rev iters

--- 42/CPP/ft_containers/reverse_iterator.hpp
+++ 42/CPP/ft_containers/reverse_iterator.hpp
@@ -18,35 +18,109 @@ namespace ft
 			typedef typename ft::iterator_traits<Iter>::pointer 			pointer;
 			typedef typename ft::iterator_traits<Iter>::reference 			reference;
 
-			constructor();
-			operator=;
-			base();
-			operator*;
-			operator->;
-			operator[];
-			operator++;
-			operator++(int);
-			operator+=;
-			operator+;
-			operator--;
-			operator--(int);
-			operator-=;
-			operator-;
+
+			/*constructors*/
+
+			reverse_iterator():current(Iter()){}
+			explicit reverse_iterator( iterator_type x ):current(x){}
+			template< class U >
+			reverse_iterator( const reverse_iterator<U>& other )
+			:current(other.base())
+			{}
+
+			/*member functions*/
+			iterator_type base() const {return current;}
+
+			template< class U >
+			reverse_iterator& operator=( const reverse_iterator<U>& other )
+			{
+				current = other.current;
+				return (*this);
+			}
+			reference operator*() const
+			{
+				Iter tmp = current;
+				return *(--tmp);
+			}
+			pointer operator->() const
+			{
+				return std::adressof(operator*());
+			}
+			reference operator[]( difference_type n ) const
+			{
+				return base()[-n-1];
+			}
+			reverse_iterator& operator++()
+			{
+				current--;
+				return (*this);
+			}
+			reverse_iterator& operator--()
+			{
+				current++;
+				return (*this);
+			}
+			reverse_iterator operator++( int )
+			{
+				Iter tmp = base();
+				++(*this);
+				return (tmp);
+			}
+			reverse_iterator operator--( int )
+			{
+				Iter tmp = base();
+				--(*this);
+				return (tmp);
+			}
+			reverse_iterator operator+( difference_type n ) const
+			{
+				iter tmp = base();
+				return (tmp - n);
+			}
+			reverse_iterator operator-( difference_type n ) const
+			{
+				iter tmp = base();
+				return (tmp + n);
+			}
+			reverse_iterator& operator+=( difference_type n )
+			{
+				current = current + n;
+				return (*this);
+			}
+			reverse_iterator& operator-=( difference_type n )
+			{
+				current = current - n;
+				return (*this);
+			}
 
 			//non members : static keyword?
 
-			operator==;
-			operator!=;
-			operator<;
-			operator<=;
-			operator>;
-			operator>=;
-			operator+;
-			operator-;
+			template< class Iterator1, class Iterator2 >
+			bool operator==( const std::reverse_iterator<Iterator1>& lhs,
+                 const std::reverse_iterator<Iterator2>& rhs );
+
+			template< class Iterator1, class Iterator2 >
+			bool operator!=( const std::reverse_iterator<Iterator1>& lhs,
+                 const std::reverse_iterator<Iterator2>& rhs );
+			
+			template< class Iterator1, class Iterator2 >
+			bool operator<( const std::reverse_iterator<Iterator1>& lhs,
+                const std::reverse_iterator<Iterator2>& rhs );
+			
+			template< class Iterator1, class Iterator2 >
+			bool operator<=( const std::reverse_iterator<Iterator1>& lhs,
+                 const std::reverse_iterator<Iterator2>& rhs );
+
+			template< class Iterator1, class Iterator2 >
+			bool operator>( const std::reverse_iterator<Iterator1>& lhs,
+                const std::reverse_iterator<Iterator2>& rhs );
+			
+			template< class Iterator1, class Iterator2 >
+			bool operator>=( const std::reverse_iterator<Iterator1>& lhs,
+                 const std::reverse_iterator<Iterator2>& rhs );
 			
 		protected:
-			Iter iter; // underlying iterator?
-
+			Iter current;
 	};
 }
 
--- 42/CPP/ft_containers/vector.hpp
+++ 42/CPP/ft_containers/vector.hpp
@@ -1,18 +1,20 @@
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 
-#include <iostream>
+# include <iostream>
+# include <string>
+
+#include <vector>
+
 
 namespace ft
 {
-	template< class T , class Allocator = std::allocator<T> >
-	class vector
-	{
-
-	};
+	template<class T,class Container = std::vector<T>> class stack;
 
 }
 
 
+//std::ostream &operator<<(std::ostream &o, vector const &instance);
 
-#endif
+
+#endif /* ************************************************************* VECTOR_HPP */
